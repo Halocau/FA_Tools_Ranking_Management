8488c699affa929d897f3c64bcd2f5f217b9380f
quatbt <quatbt@fpt.com>
2024-10-17 15:43:29 +0700

    feature: Update, delete + exception

--- backend/src/main/java/backend/controller/RankingGroupController.java
+++ backend/src/main/java/backend/controller/RankingGroupController.java
@@ -18,14 +18,17 @@ public class RankingGroupController {
     public RankingGroupController(IRankingGroupService iRankingGroupService) {
         this.iRankingGroupService = iRankingGroupService;
     }
+
     @GetMapping
     public List<RankingGroup> getAllRankingGroups() {
         return iRankingGroupService.getAllRankingGroups();
     }
+
     @GetMapping("/get/{id}")
     public RankingGroup findRankingGroupById(@PathVariable int id) {
         return iRankingGroupService.findRankingGroupById(id);
     }
+
     @PostMapping("/add")
     public ResponseEntity<RankingGroup> addRankingGroup(@RequestBody RankingGroup rankingGroup) {
         rankingGroup.setGroupId(0);
@@ -38,7 +41,35 @@ public class RankingGroupController {
         }
     }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<RankingGroup> updateRankingGroup(@RequestBody RankingGroup rankingGroup, @PathVariable int id) {
+        RankingGroup exits = iRankingGroupService.findRankingGroupById(id);
+        if (exits != null) {
+            exits.setGroupName(rankingGroup.getGroupName());
+            exits.setNumEmployees(rankingGroup.getNumEmployees());
+            exits.setCurrent_ranking_decision(rankingGroup.getCurrent_ranking_decision());
+            exits.setCreatedBy(rankingGroup.getCreatedBy());
+            exits.setCreatedAt(rankingGroup.getCreatedAt());
+            exits.setUpdatedAt(exits.getCreatedAt());
+            iRankingGroupService.updateRankingGroup(rankingGroup);
+            return ResponseEntity.ok(exits);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
 
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteRankingGroup(@PathVariable int id) {
+        RankingGroup exits = iRankingGroupService.findRankingGroupById(id);
+        if (exits != null) {
+            iRankingGroupService.deleteRankingGroup(exits);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+    }
 
 
 }
