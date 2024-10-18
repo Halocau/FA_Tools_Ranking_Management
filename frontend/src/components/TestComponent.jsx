38d9fe0c57e4d879cd1bc7677da124dd7a73a131
MeoHoang176788 <hoangmnhe176788@fpt.edu.vn>
2024-10-18 10:05:20 +0700

    Update TestComponent.jsx

--- frontend/src/components/TestComponent.jsx
+++ frontend/src/components/TestComponent.jsx
@@ -3,19 +3,12 @@ import useLogin from '../hooks/useLogin';
 import useRankingGroup from '../hooks/useRankingGroup';
 export default function TestComponent() {
 
-    const unusedVariable = "I'm unused!"; // Unused variable
-
-
     const { data: loginData, error: loginError, loading: loginLoading, login } = useLogin();
 
-    const { data: rankingGroupData, error: rankingGroupError, loading: rankingGroupLoading, addRankingGroup } = useRankingGroup();
     useEffect(() => {
         login("quatbt", "111");
     }, []);
 
-    useEffect(() => {
-        addRankingGroup({ groupName: "Test Group", createdBy: 1 });
-    }, [])
     // Log data after it has been fetched and updated
     console.log("Fetched Data:", loginData);
 
