4c2e40d0d4faebb1118858e61c80bd79f92003ea
duongluuba <duonglbq19092003@gmail.com>
2024-10-22 22:38:26 +0700

    22_10_2024_v1

--- backend/src/main/java/backend/dao/IRankingGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package backend.dao;
-
-import backend.model.RankingGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRankingGroupRepository extends JpaRepository<RankingGroup, Integer> {
-}
--- backend/src/main/java/backend/exception/AccountException.java
+++
@@ -1,7 +0,0 @@
-package backend.exception;
-
-public class AccountException extends RuntimeException {
-    public AccountException(String message) {
-        super(message);
-    }
-}
