a0b8843a9bd9b395dce583a80673f1e70a071484
Phạm Quang Duy <143075321+quangduy11@users.noreply.github.com>
2024-10-22 16:24:41 +0700

    Update Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import SignupPage from "./pages/Auth/SignupPage";
 import ForgetPasswordPage from "./pages/Auth/ForgetPasswordPage";
 import Sidebar from "./layouts/Sidebar";
 import Header from "./layouts/Header";
-import RankingGroups from "./pages/RankingGroups";
+import RankingGroups from "./components/RankingGroups";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import { AuthProvider } from "./contexts/AuthContext";
@@ -20,15 +20,25 @@ function App() {
           <Route path="/homepage" element={<HomePage />} />
           <Route path="/register" element={<SignupPage />} />
           <Route path="/forgetpassword" element={<ForgetPasswordPage />} />
-          <Route path="/ranking_group" element={
-            <>
-              <Row><Header /></Row>
-              <Row>
-                <Col md={2}><Sidebar /></Col>
+          <Route
+            path="/ranking_group"
+            element={
+              <>
+                <Row>
+                  <Header />
+                </Row>
+                <Row>
+                  <Col md={2}>
+                    <Sidebar />
+                  </Col>
 
-                <Col md={10} ><RankingGroups className="ml-2" /></Col>
-              </Row>
-            </>} />
+                  <Col md={10}>
+                    <RankingGroups className="ml-2" />
+                  </Col>
+                </Row>
+              </>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
