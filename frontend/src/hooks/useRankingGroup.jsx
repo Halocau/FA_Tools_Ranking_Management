91a67a3268a44404c5eecb848d217ee4fdde5fd4
Phạm Quang Duy <143075321+quangduy11@users.noreply.github.com>
2024-10-23 16:43:58 +0700

    Custom Modal

--- frontend/src/hooks/useRankingGroup.jsx
+++ frontend/src/hooks/useRankingGroup.jsx
@@ -62,18 +62,13 @@ const useRankingGroup = () => {
     };
 
     // HTTP DELETE request to delete a ranking group by ID
-    const deleteRankingGroup = async (id) => {
-       const confirmDelete = window.confirm(
-     "Are you sure you want to delete this group?"
-   );
-   if (confirmDelete) {
+    const deleteRankingGroup = async (id) => {          
      try {
        await http.delete(`/ranking-group/delete/${id}`);
        setData(groups.filter((dt1) => dt1.id !== id));
      } catch (error) {
        console.error("Error deleting employee:", error);
-     }
-   }
+     }   
     };
 
     // Return the data and functions to be used in components
