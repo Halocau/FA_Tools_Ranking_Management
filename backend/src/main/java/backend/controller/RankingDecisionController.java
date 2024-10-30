6b74ae0a96c3fd47c06692c99ceaabf19bd6cf8e
MeoHoang176788 <meomapgm@gmail.com>
2024-10-30 14:36:20 +0700

    Update RankingDecisionController.java

--- backend/src/main/java/backend/controller/RankingDecisionController.java
+++ backend/src/main/java/backend/controller/RankingDecisionController.java
@@ -4,13 +4,9 @@ import backend.model.dto.RankingDecisionResponse;
 import backend.model.entity.RankingDecision;
 import backend.model.form.RankingDecision.CreateRankingDecision;
 import backend.service.IRankingDecisionService;
-<<<<<<< HEAD
 
-import java.util.List;
-
-=======
 import jakarta.validation.Valid;
->>>>>>> quatbt
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-<<<<<<< HEAD
 @RequestMapping("/api/ranking-decision")
-=======
-@RequestMapping("api/ranking-decision")
->>>>>>> quatbt
 public class RankingDecisionController {
     private IRankingDecisionService iRankingDecisionService;
 
@@ -31,44 +23,27 @@ public class RankingDecisionController {
         this.iRankingDecisionService = iRankingDecisionService;
     }
 
-<<<<<<< HEAD
-    @GetMapping
-    public ResponseEntity<List<RankingDecision>> getRankingDecisions() {
-        List<RankingDecision> rankingDecisions = iRankingDecisionService.getRankingDecisions();
-        return ResponseEntity.ok().body(rankingDecisions);
-    }
-
-    @GetMapping("/get/{groupId}")
-=======
-    @GetMapping("/groupid/{groupId}")
->>>>>>> quatbt
-    public ResponseEntity<RankingDecision> findByGroupId(@PathVariable int groupId) {
-        RankingDecision decision = iRankingDecisionService.findByGroupId(groupId);
-        return ResponseEntity.ok().body(decision);
-    }
-
-<<<<<<< HEAD
-    @PutMapping("/update/{groupId}")
-=======
-    @PutMapping("/putid/{groupId}")
->>>>>>> quatbt
-    public ResponseEntity<Void> clearGroupId(@PathVariable int groupId) {
-        iRankingDecisionService.updateRankingDecisionGroupIdToNull(groupId);
-        return ResponseEntity.noContent().build();
-    }
-
-<<<<<<< HEAD
-=======
     @GetMapping
     public List<RankingDecisionResponse> getRankingDecisionList() {
         List<RankingDecision> decisionList = iRankingDecisionService.getRankingDecisions();
         return iRankingDecisionService.getRankingDecisionResponses(decisionList);
     }
+
+    @GetMapping("/groupid/{groupId}")
+    public ResponseEntity<RankingDecision> findByGroupId(@PathVariable int groupId) {
+        RankingDecision decision = iRankingDecisionService.findByGroupId(groupId);
+        return ResponseEntity.ok().body(decision);
+    }
+
+    @PutMapping("/putid/{groupId}")
+    public ResponseEntity<Void> clearGroupId(@PathVariable int groupId) {
+        iRankingDecisionService.updateRankingDecisionGroupIdToNull(groupId);
+        return ResponseEntity.noContent().build();
+    }
+
     @PostMapping("/add")
     public String addRankingDecision(@RequestBody @Valid CreateRankingDecision form) {
         iRankingDecisionService.createRankingDecision(form);
         return "Ranking Decision Added Successfully";
     }
-
->>>>>>> quatbt
 }
