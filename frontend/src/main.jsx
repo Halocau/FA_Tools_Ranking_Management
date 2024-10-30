a90642de2fd457df011e3e14c58bfcb72b426125
Phạm Quang Duy <143075321+quangduy11@users.noreply.github.com>
2024-10-30 14:55:33 +0700

    Format Frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import CssBaseline from "@mui/material/CssBaseline";
-import {
-  ThemeProvider,
-  createTheme,
-  useColorScheme,
-} from "@mui/material/styles";
-import Theme from "./components/Common/Theme.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={Theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <CssBaseline />
+    <App />
   </StrictMode>
 );
