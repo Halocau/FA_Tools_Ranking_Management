bbc940ff651d687852fe479af3232016196658d6
MeoHoang176788 <meomapgm@gmail.com>
2024-11-01 10:58:24 +0700

    Update Criteria Request

---
+++ backend/src/main/java/backend/model/form/Criteria/UpdateExistedCriteria.java
@@ -0,0 +1,13 @@
+package backend.model.form.Criteria;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class UpdateExistedCriteria {
+    @NotNull
+    private String criteriaName;
+    @NotNull
+    private Integer updatedBy;
+}
