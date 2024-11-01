9ca5f5060436f4fbbb2989781d24de99411d3f56
MeoHoang176788 <meomapgm@gmail.com>
2024-11-01 15:30:45 +0700

    Add request for criteria

--- backend/src/main/java/backend/model/form/Criteria/AddCriteriaRequest.java
+++ backend/src/main/java/backend/model/form/Criteria/AddCriteriaRequest.java
@@ -1,13 +1,12 @@
 package backend.model.form.Criteria;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class AddNewCriteria {
+public class AddCriteriaRequest {
     @NotNull
     private String criteriaName;
-    @NotNull
+
     private Integer createdBy;
 }
