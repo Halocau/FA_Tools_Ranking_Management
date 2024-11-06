fd06fc4a1c2953842ca4b6d5b7977e252cd7a276
Halocau <buitienquat2k3@gmail.com>
2024-11-07 04:19:51 +0700

    validate ranking group

--- backend/src/main/java/backend/dao/IRankingGroupRepository.java
+++ backend/src/main/java/backend/dao/IRankingGroupRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IRankingGroupRepository extends JpaRepository<RankingGroup, Integer> {
     boolean existsByGroupName(String groupName);// check group name
+    boolean existsByGroupNameAndGroupIdNot(String groupName, Integer groupId);
 }
