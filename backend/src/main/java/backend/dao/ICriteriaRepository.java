ef22379e332edc79e96e8905c5487131e7ccef10
MeoHoang176788 <meomapgm@gmail.com>
2024-11-12 14:17:21 +0700

    Update Criteria and Options
    - Add feature pagination and search for criteria and options

--- backend/src/main/java/backend/dao/ICriteriaRepository.java
+++ backend/src/main/java/backend/dao/ICriteriaRepository.java
@@ -2,10 +2,11 @@ package backend.dao;
 
 import backend.model.entity.Criteria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICriteriaRepository extends JpaRepository<Criteria, Integer> {
+public interface ICriteriaRepository extends JpaRepository<Criteria, Integer>, JpaSpecificationExecutor<Criteria> {
 }
--- backend/src/main/java/backend/model/validation/Options/AddOptionNameNotDuplicate.java
+++ backend/src/main/java/backend/model/validation/Options/AddOptionNameNotDuplicate.java
@@ -9,11 +9,12 @@ import java.lang.annotation.*;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.TYPE_USE})
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR, ElementType.TYPE_USE })
 // Chỉ định các nơi có thể áp dụng chú thích này
-@Retention(RetentionPolicy.RUNTIME)// Chỉ định chú thích này sẽ được giữ lại tại runtime
-@Documented// Cho phép hiển thị thông tin của chú thích trong tài liệu Javadoc
-@Constraint(validatedBy = {AddOptionNameNotDuplicateValidator.class})
+@Retention(RetentionPolicy.RUNTIME) // Chỉ định chú thích này sẽ được giữ lại tại runtime
+@Documented // Cho phép hiển thị thông tin của chú thích trong tài liệu Javadoc
+@Constraint(validatedBy = { AddOptionNameNotDuplicateValidator.class })
 @Repeatable(AddOptionNameNotDuplicate.List.class)
 public @interface AddOptionNameNotDuplicate {
     String message() default "{RankingGroup name exists already!}";
@@ -21,11 +22,12 @@ public @interface AddOptionNameNotDuplicate {
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
-    @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.TYPE_USE})
+
+    @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE,
+            ElementType.CONSTRUCTOR, ElementType.TYPE_USE })
     @Retention(RUNTIME)
     @Documented
     @interface List {
         AddOptionNameNotDuplicate[] value();
     }
 }
-
