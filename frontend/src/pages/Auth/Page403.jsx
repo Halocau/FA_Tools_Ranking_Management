a9e8e5132210c87204748868733f93144807001b
MeoHoang176788 <meomapgm@gmail.com>
2024-11-16 09:44:43 +0700

    Update AppRoute

--- frontend/src/pages/Auth/Page403.jsx
+++ frontend/src/pages/Auth/Page403.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
 const ForbiddenPage = () => {
+
+    const navigate = useNavigate();
+    const handleHome = () => {
+        navigate('/');
+    }
     return (
         <div style={styles.container}>
             <h1 style={styles.header}>403 Forbidden</h1>
             <p style={styles.message}>You do not have permission to access this page.</p>
-            <Link to="/" style={styles.link}>Go Back to Home</Link>
+            <Link to="/" onClick={handleHome} className="btn btn-primary" style={styles.link}>Go Back to Home</Link>
         </div>
     );
 };
