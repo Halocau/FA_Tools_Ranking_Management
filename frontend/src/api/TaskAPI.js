f23dcfa188c632eb5c07bced5aefde86a3ec0d7d
MeoHoang176788 <meomapgm@gmail.com>
2024-11-18 10:28:31 +0700

    Revert "Merge remote-tracking branch 'origin/DuyPQ' into HoangMN"
    This reverts commit 12900af76cef5fc4d43f91b5f1fc51ed5555e3e8, reversing
    changes made to 0f392ce7e782b9dfc63a06647bfe611d396ed033.

--- frontend/src/api/TaskAPI.js
+++ frontend/src/api/TaskAPI.js
@@ -1,5 +1,5 @@
 // frontend/api/taskApi.js
-import authClient from "./baseapi/AuthorAPI";
+import authClient from './authClient';
 
 const TASK_API = '/task';
 
@@ -40,7 +40,7 @@ const taskApi = {
     },
 
     // Update a task by ID
-    updateTaskByID: async (id, formData) => {
+    updateTask: async (id, formData) => {
         try {
             const response = await authClient.put(`${TASK_API}/update/${id}`, formData);
             return response.data;
@@ -62,10 +62,10 @@ const taskApi = {
     },
 
     // Search tasks by name with pagination
-    searchByTaskName: async (filter = "", page = 0, size = 5) => {
+    searchByTaskName: async (taskName = '', page = 0, size = 5) => {
         try {
-            const response = await authClient.get(`${TASK_API}`, {
-              params: { filter: filter, page: page, size: size },
+            const response = await authClient.get(`${TASK_API}/search`, {
+                params: { name: taskName, page: page, size: size },
             });
             return response.data;
         } catch (error) {
