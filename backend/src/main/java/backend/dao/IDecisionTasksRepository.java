4072e1f5d609f6bcb1a69d76667d8266473c38b7
MeoHoang176788 <meomapgm@gmail.com>
2024-11-24 22:29:12 +0700

    Revert "Merge branch 'quatbt' into HoangMN"
    This reverts commit c68f4d3b27dcccb9956243836b29afba0e3a84e4, reversing
    changes made to 1af7f389ab1937e1e7c420dc517d0e653736540d.

--- backend/src/main/java/backend/dao/IDecisionTasksRepository.java
+++ backend/src/main/java/backend/dao/IDecisionTasksRepository.java
@@ -9,12 +9,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface IDecisionTasksRepository extends JpaRepository<DecisionTasks, DecisionTasksSerializable> {
 
 
     public List<DecisionTasks> findByDecisionId(int decisionId);
-    public Optional<DecisionTasks> findByDecisionIdAndTaskId(int decisionId, int taskId);
+    public  DecisionTasks findByDecisionIdAndTaskId(int decisionId, int taskId);
 }
