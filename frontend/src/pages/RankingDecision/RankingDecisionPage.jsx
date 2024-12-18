06221220d6cec7f4e82191b691af5c842d19dce9
duongluuba <duonglbq19092003@gmail.com>
2024-12-18 20:55:13 +0700

    Merge branch 'DuyPQ' into DuongLBQ

--- frontend/src/pages/RankingDecision/RankingDecisionPage.jsx
+++ frontend/src/pages/RankingDecision/RankingDecisionPage.jsx
@@ -275,7 +275,7 @@ const RankingDecision = () => {
         { field: "index", headerName: "ID", width: 80 },
         { field: "dicisionname", headerName: "Ranking Decision Name", width: 350 },
         { field: "finalizedAt", headerName: "Finalized At", width: 200 },
-        { field: "finalizedBy", headerName: "Finalized By", width: 150 },
+        { field: "finalizeByName", headerName: "Finalized By", width: 150 },
         { field: "status", headerName: "Status", width: 130 },
         {
             field: "action",
@@ -331,7 +331,8 @@ const RankingDecision = () => {
                 dicisionname: decision.decisionName,
                 finalizedAt: decision.status === 'Finalized'
                     ? (decision.finalizedAt ? decision.finalizedAt.slice(0, 10) : '-')
-                    : '-', finalizedBy: decision.status === 'Finalized' ? (decision.finalizedBy == null ? "-" : decision.finalizedBy) : '-',
+                    : '-',
+                finalizeByName: decision.status === 'Finalized' ? (decision.finalizeByName == null ? "-" : decision.finalizeByName) : '-',
                 status: decision.status
             }));
             setRows(mappedRows); // Update rows with data from decisions
