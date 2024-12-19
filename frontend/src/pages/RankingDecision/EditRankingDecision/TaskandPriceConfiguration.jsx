b05a25ac1cfe416bba4474504b985c11583e3ebd
duongluuba <duonglbq19092003@gmail.com>
2024-12-19 15:22:37 +0700

    19_12

--- frontend/src/pages/RankingDecision/EditRankingDecision/TaskandPriceConfiguration.jsx
+++ frontend/src/pages/RankingDecision/EditRankingDecision/TaskandPriceConfiguration.jsx
@@ -378,7 +378,7 @@ const TaskandPriceConfiguration = ({ decisionStatus, goToNextStep, showErrorMess
                                         }
                                         return (
                                             <TextField
-                                                value={editedWages[`${taskId}-${rankingTitleId}-${wageType}`] || defaultValue || ''}
+                                                value={defaultValue ? defaultValue : 0}
                                                 onChange={(e) => {
                                                     const value = e.target.value;
                                                     if (/^\d*$/.test(value)) {
