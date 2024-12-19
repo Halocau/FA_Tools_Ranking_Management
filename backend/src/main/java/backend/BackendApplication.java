5cb3f6bf847d599f5ceadc7342bdbc94ece761b1
Halocau <buitienquat2k3@gmail.com>
2024-12-19 10:22:34 +0700

    update infomation group

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -2,7 +2,12 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Group 2 - Project Base - HN24_CPL_PJB_05
+ * Mentor: Nguyễn Duy Thành
+ * Leader: Bùi Tiến Quát
+ * Member: Bùi Tiến Quát (Halocau), Mai Nhật Hoàng (MeoHoang3017), Phạm Quang Duy (quangduy11), Lê Bá Quý Dương (duongluuba), Hoàng Văn Vũ (heloo2909)
+ * */
 @SpringBootApplication
 public class BackendApplication {
 
